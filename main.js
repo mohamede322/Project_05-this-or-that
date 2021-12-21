b282da5b48feb8d741ea1ac74607a5aed7398546
Psycho <93525954+itspsycholol322@users.noreply.github.com>
2021-12-21 11:43:08 +0200

    fixed fetch

--- main.js
+++ main.js
@@ -2,23 +2,29 @@ let thisAPI = "https://dog.ceo/api/breeds/image/random";
 let thatAPI = "https://dog.ceo/api/breeds/image/random";
 let thisDog = document.querySelector(".this");
 let thatDog = document.querySelector(".that");
-
 let btnS = document.querySelectorAll("button");
 
 btnS.forEach((btn) => {
   btn.addEventListener("click", () => {
-    location.reload();
+    if (btn.className === "this") {
+      getThisRandomDog();
+    } else if (btn.className === "that") {
+      getThatRandomDog();
+    }
   });
 });
 
-async function getRandomDog() {
+async function getThisRandomDog() {
   let thisRes = await fetch(thisAPI);
   let thisData = await thisRes.json();
 
+  thisDog.src = thisData.message;
+}
+async function getThatRandomDog() {
   let thatRes = await fetch(thatAPI);
   let thatData = await thatRes.json();
 
-  thisDog.src = thisData.message;
   thatDog.src = thatData.message;
 }
-getRandomDog();
+getThisRandomDog();
+getThatRandomDog();
